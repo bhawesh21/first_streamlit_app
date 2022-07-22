fa402bcda43fa757ff3a823402020758c03262aa
bhawesh21 <108086648+bhawesh21@users.noreply.github.com>
2022-07-22 16:06:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())  # arr
 streamlit.dataframe(fruityvice_normalized) # puts the values in  the table
 
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
